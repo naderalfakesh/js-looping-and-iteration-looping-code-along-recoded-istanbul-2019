e85b86dacb4473ecd69a4a002a4e9b2c1fc5fec3
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-01-18 19:54:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 function writeCards(names,event) {
   let result=[];
-  for (let i = 0; i < cards.length; i++) {
+  for (let i = 0; i < names.length; i++) {
     result.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
-
+  return result;
 }
 
 writeCards(["Ada", "Brendan", "Ali"], "birthday");
