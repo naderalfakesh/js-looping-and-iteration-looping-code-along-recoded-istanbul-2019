986bc8a57d99d8b43c8914d2ae17d44692a9f0d6
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-01-18 19:53:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 
-function writeCards(cards,event) {
+function writeCards(names,event) {
   let result=[];
   for (let i = 0; i < cards.length; i++) {
     result.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
